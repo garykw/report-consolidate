d28cd99114457cd68e34e2893aadcb46ad6b8459
Gary Wong <[01;31m[K"email"[m[K: "garykwong2016@gmail.com>
2017-07-14 23:15:00 -0700

    Initial commit

---
+++ src/app/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyDcCJTAMZauy6ckZsteyPw92fByAsyDrlo",
+        authDomain: "report-organizer.firebaseapp.com",
+        databaseURL: "https://report-organizer.firebaseio.com",
+        projectId: "report-organizer",
+        storageBucket: "",
+        messagingSenderId: "657834674629"
+    },
+};
