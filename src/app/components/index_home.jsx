f5d60ed10bfbb599d4086204ca98727b6cc87a94
Gary Wong <[01;31m[K"email"[m[K: "garykwong2016@gmail.com>
2017-07-16 22:17:31 -0700

    Change implementation of dichart render

--- src/app/components/index_home.jsx
+++ src/app/components/index_home.jsx
@@ -3,6 +3,13 @@ import React, {Component, PropTypes} from "react"
 var Papa = require('./../../papaparse.min.js');
 var file = require('file-system');
 
+
+function sortByTime(dataArray){
+  return dataArray.sort((a,b)=>{
+	   return a.time < b.time ? -1 : a.time > b.time ? 1 : 0;
+  })
+}
+
 export default class ApplicationWrapper extends Component {
   constructor(props) {
     super(props)
@@ -22,6 +29,9 @@ export default class ApplicationWrapper extends Component {
   }
 
   handleParsedData = (results) => {
+    // let { results } = this.state
+    // debugger
+    //
     this.setState({fileName: '', results})
   }
 
@@ -44,62 +54,181 @@ export default class ApplicationWrapper extends Component {
     this.setState({uploadedFile: uploadedFile, fileName: currentFile.name})
   }
 
+  handleSortingResult( sortedResult ) {
+
+    var transactionBase = {
+      // date {
+      //   amountUnit: {
+      //     balance: null,
+      //     change: null,
+      //     fees: 0
+      //   }
+      // }
+    }
+
+    var currentDate = null
+    var unitType = null
+
+    sortedResult.map(item => {
+
+      // if (item.balance == 3233.6770590468177){
+      //   debugger
+      // }
+      //
+      if (!item.time || !item.type || !item.balance || !item.amount){
+        return
+      }
+
+      // if (item.type == 'fee') {
+      //   totalFee += item.amount
+      // }
+
+      // if (currentDate != null && currentDate != item.time.replace(/T.*/, '') ){
+      if (currentDate != null && transactionBase[item.time.replace(/T.*/, '')] == undefined){
+        currentDate = null
+        unitType = null
+      }
+
+      // create new date
+      if (currentDate == null) {
+        currentDate = item.time.replace(/T.*/, '')
+
+        unitType = item['amount/balance unit']
+
+        transactionBase[currentDate] = {}
+        transactionBase[currentDate][unitType] = {
+          balance: item.balance,
+        }
+
+        transactionBase[currentDate][unitType]['change'] = ( item.type != 'fee' ) ? item.amount : 0
+        transactionBase[currentDate][unitType]['fees'] = ( item.type == 'fee' ) ? item.amount : 0
+
+      } else if ( currentDate == item.time.replace(/T.*/, '') ) {
+
+        // add to balance
+        var currentDateUnit = transactionBase[currentDate][item['amount/balance unit']]
+
+        if (currentDateUnit){
+
+          if (item.type == 'fee') {
+            currentDateUnit['fees'] += item.amount
+          } else {
+            currentDateUnit['change'] += item.amount
+          }
+
+          currentDateUnit['balance'] = item.balance
+
+        } else {
+          // different unit
+         currentDateUnit = {
+           balance: item.balance
+          }
+
+          currentDateUnit['change'] = ( item.type != 'fee' ) ? item.amount : 0
+          currentDateUnit['fees'] = ( item.type == 'fee' ) ? item.amount : 0
+        }
+
+      }
+    })
+
+    return transactionBase
+  }
+
   renderParseData = () => {
     let {results} = this.state
 
       var totalFee = 0
-        var currentDate = null, currentDateFees = 0
-        var currentBalance, currentChange, renderDate, renderDateFees, renderBalance, renderChange, amountUnit
+      var currentDate = null, currentDateFees = 0, hasConverted = false
+      var currentBalance, currentChange, renderDate, renderDateFees, renderBalance, renderChange, amountUnit, convertedBalance, convertedChange, convertedUnit
 
-        var hasRendered = false
+      var sortedOranizedData = this.handleSortingResult( sortByTime(results.data) )
 
-        var el = results.data.map(item => {
-          if (item.type == 'fee') {
-            totalFee += item.amount
-          }
+      var el = Object.keys(sortedOranizedData).map( dateTime => {
+        var units = Object.keys( sortedOranizedData[dateTime] )
 
-          if (!item.time || !item.type || !item.balance || !item.amount){
-            return
-          }
+        // var unitTypes = dateTime.keySeq().toArray()
+        if (units.length == 1){
+          var currentUnit = sortedOranizedData[dateTime][units[0]]
 
-          if (currentDate == null) {
-            // inital setup
-            amountUnit = item['amount/balance unit']
-            currentDate = item.time.replace(/T.*/, '')
-            currentChange = item.amount
-            currentBalance = item.balance
+          return (
+            <tr>
+              <td>
+                {dateTime}
+              </td>
+              <td>
+                { Math.round((currentUnit.change - currentUnit.fees) * 100) / 100 }
+              </td>
+              <td>
+                { Math.round(currentUnit.fees * 100) / 100 }
+              </td>
+              <td>
+                { Math.round(currentUnit.balance * 100) / 100}
+              </td>
+            </tr>
+          )
 
-            if (item.type == 'fee') {
-              currentDateFees = item.amount
-            }
-
-          } else if (currentDate == item.time.replace(/T.*/, '')) {
-            // same date
-            currentChange += item.amount
-            currentBalance = item.balance
-
-            if (item.type == 'fee') {
-              currentDateFees += item.amount
-            }
-
-          } else {
-            // same new date
-            renderDate = currentDate
-            renderChange = currentChange
-            renderBalance = currentBalance
-            renderDateFees = currentDateFees
-
-            currentDate = item.time.replace(/T.*/, '')
-            currentChange = item.amount
-            currentBalance = item.balance
-
-            if (item.type == 'fee') {
-              currentDateFees = item.amount
-            }
-
-            return this.handleRenderRow(renderDate, renderBalance, renderChange, renderDateFees)
-          }
-        })
+        }
+        // }
+      })
+      // return sortedResult.map(item => {
+      //   if (!item.time || !item.type || !item.balance || !item.amount){
+      //     return
+      //   }
+      //
+      //   // if (item.type == 'fee') {
+      //   //   totalFee += item.amount
+      //   // }
+      //   //
+      //
+      //   // inital setup
+      //   if (currentDate == null) {
+      //     amountUnit = item['amount/balance unit']
+      //     currentDate = item.time.replace(/T.*/, '')
+      //     currentChange = item.amount
+      //     currentBalance = item.balance
+      //
+      //     if (item.type == 'fee') {
+      //       currentDateFees = item.amount
+      //     }
+      //
+      //   } else if (currentDate == item.time.replace(/T.*/, '')) {
+      //     // same date
+      //     currentChange += item.amount
+      //     currentBalance = item.balance
+      //
+      //     if (item.type == 'fee') {
+      //       currentDateFees += item.amount
+      //     }
+      //
+      //     if (amountUnit != item['amount/balance unit'] ) {
+      //       convertedUnit = item['amount/balance unit']
+      //
+      //       if (!convertedBalance || !convertedChange ){
+      //
+      //       } else {
+      //
+      //       }
+      //
+      //     }
+      //
+      //   } else {
+      //     // same new date
+      //     renderDate = currentDate
+      //     renderChange = currentChange
+      //     renderBalance = currentBalance
+      //     renderDateFees = currentDateFees
+      //
+      //     currentDate = item.time.replace(/T.*/, '')
+      //     currentChange = item.amount
+      //     currentBalance = item.balance
+      //
+      //     if (item.type == 'fee') {
+      //       currentDateFees = item.amount
+      //     }
+      //
+      //     return this.handleRenderRow(renderDate, renderBalance, renderChange, renderDateFees)
+      //   }
+      // })
 
         return <div>
           <table className='table table-striped'>
@@ -114,17 +243,18 @@ export default class ApplicationWrapper extends Component {
             <tbody>
 
               {el}
-              { this.handleRenderRow(currentDate, currentBalance, currentChange, currentDateFees) }
+              {/* { this.handleRenderRow(currentDate, currentBalance, currentChange, currentDateFees) } */}
 
             </tbody>
           </table>
-          <div>Unit: {amountUnit}</div>
-          { amountUnit == "USD" ? <div>Total Fees: {totalFee}</div> : null }
+          {/* <div>Unit: {amountUnit}</div> */}
+          {/* { amountUnit == "USD" ? <div>Total Fees: {totalFee}</div> : null } */}
         </div>
 
       }
 
-      handleRenderRow(date, balance, change, fees){
+      // handleRenderRow(date, balance, change, fees){
+      handleRenderRow(dateObject){
 
         return (
           <tr>
